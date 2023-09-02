95ab26f504eec14236afca8c102e5a5719db59b3
zoro <zoro@qinarmy.io>
2023-09-02 08:21:47 +0800

    modify README.md

--- src/test/java/io/jdbd/mysql/simple/SimpleTests.java
+++ src/test/java/io/jdbd/mysql/simple/SimpleTests.java
@@ -7,6 +7,7 @@ import io.jdbd.session.DatabaseSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import reactor.core.publisher.Mono;
 
@@ -20,10 +21,10 @@ public class SimpleTests {
 
     private static final Logger LOG = LoggerFactory.getLogger(SimpleTests.class);
 
+    private static DatabaseSessionFactory sessionFactory;
 
-    @Test
-    public void howToStart() {
-
+    @BeforeClass
+    public void createSessionFactory() {
         final String url;
         final Map<String, Object> map = new HashMap<>();
 
@@ -32,9 +33,11 @@ public class SimpleTests {
         map.put(Driver.USER, "army_w");
         map.put(Driver.PASSWORD, "army123");
 
-        final DatabaseSessionFactory sessionFactory;
         sessionFactory = Driver.findDriver(url).forDeveloper(url, map);
+    }
 
+    @Test
+    public void howToStart() {
         final String sql;
         sql = "INSERT INTO mysql_types(my_boolean,my_bigint,my_datetime,my_datetime6,my_var_char200) VALUES (?,?,?,?,?)";
         final ResultStates resultStates;
@@ -53,6 +56,36 @@ public class SimpleTests {
         Assert.assertNotNull(resultStates);
         Assert.assertEquals(resultStates.affectedRows(), 1);
         LOG.info("auto generate id : {}", resultStates.lastInsertedId());
+    }
+
+    @Test
+    public void multiRowInsert() {
+        final String sql;
+        sql = "INSERT INTO mysql_types(my_boolean,my_bigint) VALUES (?,?),(?,?)";
+
+        final ResultStates resultStates;
+        resultStates = Mono.from(sessionFactory.localSession())
+                .flatMap(session -> session.bindStatement(sql)
+                        // first row
+                        .bind(0, JdbdType.BOOLEAN, true)
+                        .bind(1, JdbdType.BIGINT, 8888)
+                        // second row
+                        .bind(2, JdbdType.BOOLEAN, true)
+                        .bind(3, JdbdType.BIGINT, 6666)
+                        .executeUpdate(Mono::from)
+                )
+                .block();
+
+        Assert.assertNotNull(resultStates);
+        final long affectedRows = resultStates.affectedRows();
+        Assert.assertEquals(affectedRows, 2);
+
+        long lastInsertId = resultStates.lastInsertedId(); // NOTE : lastInsertId is first row id ,not last row id
+        for (int i = 0; i < affectedRows; i++) {
+            LOG.info("number {} row id: {}", i + 1, lastInsertId);
+            lastInsertId++;
+        }
+
     }
 
 
